1ee9102aeae26b48ebc05f49dcc4009f6afc9e2f
Yuriy Luchinin <luchyv2@gmail.com>
2024-04-03 22:38:49 +0300

    * добавлены тестовые аннотации AfterSuite, BeforeSuite, AfterTest, BeforeTest, Test * добавлены первоначальный gitignore и pom * добавлен корректный тестовый класс PositiveTests * добавлен тестовый класс, содержащий ошибки NegativeTests * добавлен enum TestAnnotationEnum со списком аннотаций * добавлен класс TestRunner для запуска тестов * добавлен класс Main, содержащий точку входа

---
+++ src/main/java/ru/inno/pro/lesson2/annotations/Test.java
@@ -0,0 +1,13 @@
+package ru.inno.pro.lesson2.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+
+  int priority() default 5;
+}
