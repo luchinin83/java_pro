52239a972d88551e576c634f70893c02f18e4707
Yuriy Luchinin <luchyv2@gmail.com>
2024-04-06 00:11:59 +0300

    * упростил реализацию, переписав на простые циклы и списки * удалил Enum * удалил лишние классы и методы

---
+++ src/main/java/ru/inno/pro/homework_1/Main.java
@@ -0,0 +1,11 @@
+package ru.inno.pro.homework_1;
+
+import static ru.inno.pro.homework_1.TestRunner.runTests;
+
+public class Main {
+
+  public static void main(String[] args) {
+    runTests(PositiveTests.class);
+  }
+
+}
